f5dd5215a81a512811cc0c7aa22c4f22944b4ed3
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2025-06-05 13:36:48 +0530

    turbo.json change

--- apps/web/app/btn/page.tsx
+++ apps/web/app/btn/page.tsx
@@ -1,15 +1,14 @@
-"use client"
-import { signIn } from "next-auth/react";
-import {useSearchParams} from "next/navigation";
-
-export default function Home() {
-
-    const searchParams= useSearchParams();
-    const callbackUrl = searchParams.get('callbackUrl') || '/'
-    console.log(`callbackUrllllllllllllllllllll  ${callbackUrl}}`)
-    return (
-        <button onClick={() => signIn('resend', { email: 'kushalrajpareek1@gmail.com',callbackUrl })}>
-            Sign in with Email
-        </button>
-    );
-}
+// "use client"
+// import { signIn } from "next-auth/react";
+// import {useSearchParams} from "next/navigation";
+//
+// export default function Home() {
+//
+//     const searchParams= useSearchParams();
+//     const callbackUrl = searchParams.get('callbackUrl') || '/'
+//     return (
+//         <button onClick={() => signIn('resend', { email: 'kushalrajpareek1@gmail.com',callbackUrl })}>
+//             Sign in with Email
+//         </button>
+//     );
+// }
